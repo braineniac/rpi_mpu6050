ff999f313e0c5dde2e465f39794ab9e25b68b806
Daniel Hammer <braineniac@protonmail.com>
2019-02-23 19:09:07 +0000

    Fixed bugs, seems to be working

--- src/mpu6050_node.cpp
+++ src/mpu6050_node.cpp
@@ -20,70 +20,72 @@
 #include <vector>
 #include <array>
 
-#define AVG_BUF_SIZE 400 //shoulbe less than time_offset
+#define AVG_BUF_SIZE 400 // offset buffer size after warmup
 
 class MPU6050Node {
-public:
+  public:
     MPU6050Node();
     size_t getSampleRate() { return sample_rate; }
-    void poll();
-private:
+    void   poll();
+
+  private:
     ros::NodeHandle nh;
-    int sample_rate;
-    std::string frame_id;
+    int             sample_rate;
+    std::string     frame_id;
 
     ros::Publisher data_pub;
     ros::Publisher euler_pub;
     ros::Publisher mag_pub;
 
     // Time
-    ros::Time begin;
-    ros::Time now;
-    ros::Duration warmup_time {50.0};
+    ros::Time     begin;
+    ros::Time     now;
+    ros::Duration warmup_time{50.0};
 
     // flags
-    bool warmup_done {false};
-    bool avg_done {false};
-    bool DMP_ready {false};
+    bool warmup_done{false};
+    bool avg_done{false};
+    bool DMP_ready{false};
 
     // MPU6050
-    MPU6050 mpu;
-    bool ado {false};
-    uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-    uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-    uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-    uint16_t fifoCount;     // count of all bytes currently in FIFO
-    uint8_t fifoBuffer[64]; // FIFO storage buffer
+    MPU6050  mpu;
+    bool     ado{false};
+    uint8_t  mpuIntStatus;    // holds actual interrupt status byte from MPU
+    uint8_t  devStatus;       // return status after each device operation (0 = success, !0 = error)
+    uint16_t packetSize;      // expected DMP packet size (default is 42 bytes)
+    uint16_t fifoCount;       // count of all bytes currently in FIFO
+    uint8_t  fifoBuffer[64];  // FIFO storage buffer
 
     // covariances
-    double angular_velocity_covariance {0.0};
-    double linear_acceleration_covariance {0.0};
-    double pitch_roll_covariance {0.0};
-    double yaw_covariance {0.0};
+    double angular_velocity_covariance{0.0};
+    double linear_acceleration_covariance{0.0};
+    double pitch_roll_covariance{0.0};
+    double yaw_covariance{0.0};
 
     // deviations
-    double linear_acceleration_stdev {0.0};
-    double angular_velocity_stdev {0.0};
-    double pitch_roll_stdev {0.0};
-    double yaw_stdev {0.0};
+    double linear_acceleration_stdev{0.0};
+    double angular_velocity_stdev{0.0};
+    double pitch_roll_stdev{0.0};
+    double yaw_stdev{0.0};
 
     // offsets
     std::array<std::vector<double>, AVG_BUF_SIZE> a_offset_array;
     std::array<std::vector<double>, AVG_BUF_SIZE> g_offset_array;
+
     std::array<double, 6> avg;
-    std::vector<double> a_offset;
-    std::vector<double> g_offset;
+    std::vector<double>   a_offset;
+    std::vector<double>   g_offset;
 
     // orientation/motion vars
-    Quaternion q;           // [w, x, y, z]         quaternion container
-    VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-    VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-    VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-    VectorFloat gravity;    // [x, y, z]            gravity vector
-    float euler[3];         // [psi, theta, phi]    Euler angle container
-    float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-    float la[3];            // [x, y, z]
-    float av[3];
+    Quaternion  q;           // [w, x, y, z]         quaternion container
+    VectorInt16 aa;          // [x, y, z]            accel sensor measurements
+    VectorInt16 aaReal;      // [x, y, z]            gravity-free accel sensor measurements
+    VectorInt16 aaWorld;     // [x, y, z]            world-frame accel sensor measurements
+    VectorFloat gravity;     // [x, y, z]            gravity vector
+    float       euler[3];    // [psi, theta, phi]    Euler angle container
+    float       ypr[3];      // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+    float       la[3];       // [x, y, z]
+    float       av[3];
 
     // methods
     bool mpu_setup();
@@ -100,8 +102,8 @@ MPU6050Node::MPU6050Node() {
     nh.param<std::string>("frame_id", frame_id, "base_imu");
     nh.param("sample_rate", sample_rate, 50);
     nh.param("ado", ado, false);
-    // NOISE PERFORMANCE: Power Spectral Density @10Hz, AFS_SEL=0 & ODR=1kHz 400 ug/√Hz (probably wrong)
-    nh.param("linear_acceleration_stdev", linear_acceleration_stdev, (400 / 1000000.0) * 9.807 );
+    // NOISE PERFORMANCE: Power Spectral Density @10Hz, AFS_SEL=0 & ODR=1kHz 400 ug/√Hz
+    nh.param("linear_acceleration_stdev", linear_acceleration_stdev, (400 / 1000000.0) * 9.807);
     // Total RMS Noise: DLPFCFG=2 (100Hz) 0.05 º/s-rms (probably lower (?) @ 42Hz)
     nh.param("angular_velocity_stdev", angular_velocity_stdev, 0.05 * (M_PI / 180.0));
     // 1 degree for pitch and roll
@@ -110,15 +112,15 @@ MPU6050Node::MPU6050Node() {
     nh.param("yaw_stdev", yaw_stdev, 5.0 * (M_PI / 180.0));
 
     /* setting publishers */
-    data_pub = nh.advertise<sensor_msgs::Imu>("imu/data", 50);
-    euler_pub = nh.advertise<geometry_msgs::Vector3Stamped>("imu/euler", 50);
-    mag_pub = nh.advertise<geometry_msgs::Vector3Stamped>("imu/mag", 50);
+    data_pub  = nh.advertise<sensor_msgs::Imu>("imu/data", sample_rate);
+    euler_pub = nh.advertise<geometry_msgs::Vector3Stamped>("imu/euler", sample_rate);
+    mag_pub   = nh.advertise<geometry_msgs::Vector3Stamped>("imu/mag", sample_rate);
 
     /* setting covariances */
-    angular_velocity_covariance = angular_velocity_stdev  * angular_velocity_stdev;
+    angular_velocity_covariance    = angular_velocity_stdev * angular_velocity_stdev;
     linear_acceleration_covariance = linear_acceleration_stdev * linear_acceleration_stdev;
-    pitch_roll_covariance = pitch_roll_stdev * pitch_roll_stdev;
-    yaw_covariance = yaw_stdev * yaw_stdev;
+    pitch_roll_covariance          = pitch_roll_stdev * pitch_roll_stdev;
+    yaw_covariance                 = yaw_stdev * yaw_stdev;
 
     /* Initialising vectors */
     a_offset_array.fill(std::vector<double>(3, 0.0));
@@ -128,11 +130,10 @@ MPU6050Node::MPU6050Node() {
     g_offset = std::vector<double>(3, 0.0);
 
     /* Initialising the MPU */
-    if(mpu_setup()) {
+    if(!mpu_setup()) {
         ROS_ERROR("MPU6050 setup failed, exiting...\n");
         ros::shutdown();
     }
-
 }
 
 bool MPU6050Node::mpu_setup() {
@@ -140,18 +141,20 @@ bool MPU6050Node::mpu_setup() {
     I2Cdev::initialize();
 
     mpu = MPU6050(ado ? 0x69 : 0x68);
-    if(!mpu.testConnection()){
+    if(!mpu.testConnection()) {
         std::cout << "MPU6050 connection failed" << std::endl << std::flush;
         return DMP_ready;
     }
 
-    mpu.initialize();
+    devStatus = mpu.dmpInitialize();
 
-    if(mpu.dmpInitialize()) {
+    if(devStatus) {
         ROS_ERROR("DMP Initialization failed\n");
         return DMP_ready;
     }
 
+    mpuIntStatus = mpu.getIntStatus();
+    packetSize   = mpu.dmpGetFIFOPacketSize();
     mpu.setDMPEnabled(true);
     DMP_ready = true;
     return DMP_ready;
@@ -165,6 +168,7 @@ void MPU6050Node::poll() {
     fifoCount = mpu.getFIFOCount();
     if(fifoCount >= 1024) {
         mpu.resetFIFO();
+        return;
     }
     mpu.getFIFOBytes(fifoBuffer, packetSize);
     mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -178,24 +182,26 @@ void MPU6050Node::poll() {
     // and http://docs.ros.org/api/sensor_msgs/html/msg/Imu.html
     // should be in m/s^2.
     // 1g = 9.80665 m/s^2, so we go arbitrary -> g -> m/s^s
-    la[0] = aaReal.x * 1/16384. * 9.80665 + a_offset[0];
-    la[1] = aaReal.y * 1/16384. * 9.80665 + a_offset[1];
-    la[2] = aaReal.z * 1/16384. * 9.80665 + a_offset[2];
+    la[0] = aaReal.x * 1 / 16384. * 9.80665 + a_offset[0];
+    la[1] = aaReal.y * 1 / 16384. * 9.80665 + a_offset[1];
+    la[2] = aaReal.z * 1 / 16384. * 9.80665 + a_offset[2];
 
     av[0] = ypr[2] + g_offset[0];
     av[1] = ypr[1] + g_offset[1];
     av[2] = ypr[0] + g_offset[2];
 
-    if(wait_for_warmup()){
-        if(set_offsets()) {
-            publish_imu();
-        }
+    if(wait_for_warmup()) {
+        if(set_offsets()) { publish_imu(); }
     }
 }
 
 bool MPU6050Node::wait_for_warmup() {
 
-    if((now-begin-warmup_time).toSec() > 0) { warmup_done = true; }
+    if((now - begin - warmup_time).toSec() > 0 && !warmup_done) {
+
+        ROS_INFO("Warmup finished");
+        warmup_done = true;
+    }
 
     return warmup_done;
 }
@@ -203,7 +209,7 @@ bool MPU6050Node::wait_for_warmup() {
 bool MPU6050Node::set_offsets() {
 
     // fill offset vector arrays
-    if ((now-begin-warmup_time).toSec() - int(AVG_BUF_SIZE/sample_rate+1) < 0) {
+    if((now - begin - warmup_time).toSec() - int(AVG_BUF_SIZE / sample_rate + 1) < 0) {
 
         for(int i = 0; i < a_offset_array.size(); i++) {
             // add only sane values, leave the rest 0.0
@@ -218,43 +224,45 @@ bool MPU6050Node::set_offsets() {
     }
 
     if(!avg_done) {
-    // calculate average values from offset vector arrays
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < a_offset_array.size(); j++) {
-            avg[i] += a_offset_array[j][i];
-            avg[i + 3] += g_offset_array[j][i];
+        // calculate average values from offset vector arrays
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < a_offset_array.size(); j++) {
+                avg[i] += a_offset_array[j][i];
+                avg[i + 3] += g_offset_array[j][i];
+            }
+            avg[i]     = avg[i] / a_offset_array.size();
+            avg[i + 3] = avg[i + 3] / g_offset_array.size();
         }
-        avg[i] = avg[i] / a_offset_array.size();
-        avg[i + 3] = avg[i + 3] / g_offset_array.size();
-    }
 
-    // set offset vectors
-    a_offset[0] = -avg[0];
-    a_offset[1] = -avg[1];
-    a_offset[2] = -avg[2];
-    g_offset[0] = -avg[3];
-    g_offset[1] = -avg[4];
-    g_offset[2] = -avg[5];
+        // set offset vectors
+        a_offset[0] = -avg[0];
+        a_offset[1] = -avg[1];
+        a_offset[2] = -avg[2];
+        g_offset[0] = -avg[3];
+        g_offset[1] = -avg[4];
+        g_offset[2] = -avg[5];
 
-    avg_done = true;
+        ROS_INFO("Setting offsets");
+        ROS_INFO("Setup finished");
+
+        avg_done = true;
     }
 
     return avg_done;
-
 }
 
 void MPU6050Node::publish_imu() {
 
     sensor_msgs::Imu imu_msg;
-    imu_msg.header.stamp = now;
+    imu_msg.header.stamp    = now;
     imu_msg.header.frame_id = frame_id;
 
     geometry_msgs::Vector3Stamped imu_euler_msg;
-    imu_euler_msg.header.stamp = now;
+    imu_euler_msg.header.stamp    = now;
     imu_euler_msg.header.frame_id = frame_id;
 
     geometry_msgs::Vector3Stamped mag_msg;
-    mag_msg.header.stamp = now;
+    mag_msg.header.stamp    = now;
     mag_msg.header.frame_id = frame_id;
 
     imu_msg.orientation.x = q.x;
@@ -285,19 +293,28 @@ void MPU6050Node::publish_imu() {
     data_pub.publish(imu_msg);
 }
 
-int main(int argc, char **argv){
+void SigintHandler(int sig) {
+
+    ROS_INFO("Shutting down mpu6050_node");
+
+    ros::shutdown();
+}
+
+int main(int argc, char** argv) {
 
     ros::init(argc, argv, "mpu6050_node");
-    ROS_INFO("Starting mpu6050_node...");
+    ROS_INFO("Starting mpu6050_node");
+    ROS_INFO("Warming up");
+
+    signal(SIGINT, SigintHandler);
 
     MPU6050Node mpu6050_node;
 
     ros::Rate r(mpu6050_node.getSampleRate());
 
-    while(ros::ok()){
+    while(ros::ok()) {
         mpu6050_node.poll();
         ros::spinOnce();
         r.sleep();
     }
-
 }
